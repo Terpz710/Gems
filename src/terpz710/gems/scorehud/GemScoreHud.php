3ba35f0b8c71e36b4bf9963fd71de94e6431cc84
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2025-01-04 08:54:37 -0800

    Update GemScoreHud.php

--- src/terpz710/gems/scorehud/GemScoreHud.php
+++ src/terpz710/gems/scorehud/GemScoreHud.php
@@ -4,6 +4,43 @@ declare(strict_types=1);
 
 namespace terpz710\gems\scorehud;
 
+use pocketmine\player\Player;
+
+use terpz710\gems\manager\GemManager;
+
+use Ifera\ScoreHud\ScoreHud;
+use Ifera\ScoreHud\scoreboard\ScoreTag;
+use Ifera\ScoreHud\event\TagsResolveEvent;
+use Ifera\ScoreHud\event\PlayerTagsUpdateEvent;
+
 final class GemScoreHud {
 
+    private $plugin;
+
+    public function __construct() {
+        $this->plugin = Gems::getInstance();
+    }
+
+    public function updateScoreTag(Player $player) : void{
+        if (class_exists(ScoreHud::class)) {
+            $balance = GemManager::getInstance()->seeGemBalance($player);
+            $ev = new PlayerTagsUpdateEvent(
+                $player,
+                [
+                    new ScoreTag("gems.balance", (string)$balance),
+                ]
+            );
+            $ev->call();
+        }
+    }
+
+    public function onTagResolve(TagsResolveEvent $event) : void{
+        $player = $event->getPlayer();
+        $tag = $event->getTag();
+        $balance = GemManager::getInstance()->seeGemBalance($player);
+        match ($tag->getName()) {
+            "gems.balance" => $tag->setValue((string)$balance),
+            default => null,
+        };
+    }
 }
