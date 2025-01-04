ed4dbdd0d2dede2ffd02fbed52cbbd318fec164f
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2025-01-04 09:05:14 -0800

    Update EventListener.php

--- src/terpz710/gems/EventListener.php
+++ src/terpz710/gems/EventListener.php
@@ -6,6 +6,9 @@ namespace terpz710\gems;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\entity\EntityTeleportEvent;
+
+use terpz710\gems\scorehud\GemScoreHud;
 
 class EventListener implements Listener {
 
@@ -19,11 +22,22 @@ class EventListener implements Listener {
         $player = $event->getPlayer();
         $gemManager = $this->plugin->getGemManager();
         $startAmount = $this->plugin->getConfig()->get("starting_gem_amount");
+        $scoreHud = new GemScoreHud();
 
         if (!$gemManager->hasGemBalance($player)) {
             $gemManager->createGemBalance($player, $startAmount);
         }
 
         $gemManager->updatePlayerName($player);
+        $scoreHud->updateScoreTag($player);
+    }
+
+    public function teleport(EntityTeleportEvent $event) : void{
+        $entity = $event->getEntity();
+        $scoreHud = new GemScoreHud();
+
+        if ($entity instanceof Player) {
+            $scoreHud->updateScoreTag($entity);
+        }
     }
 }
